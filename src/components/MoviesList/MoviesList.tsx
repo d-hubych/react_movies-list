e86242c0f475d3c82925d3627f0323651f01e55a
Dmytro Hubych <dh.hubych@gmail.com>
2022-10-18 09:30:05 +0300

    reset task in new branch 'develop-2.0' for remember this topic

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,16 +1,6 @@
 import React from 'react';
 import './MoviesList.scss';
-import { MovieCard } from '../MovieCard';
-import { Movie } from '../../types/Movie';
 
-type Props = {
-  movies: Movie[];
-};
-
-export const MoviesList: React.FC<Props> = ({ movies }) => (
-  <div className="movies">
-    {movies.map((movie) => (
-      <MovieCard movie={movie} key={movie.imdbId} />
-    ))}
-  </div>
+export const MoviesList: React.FC = () => (
+  <>Put the list here</>
 );
